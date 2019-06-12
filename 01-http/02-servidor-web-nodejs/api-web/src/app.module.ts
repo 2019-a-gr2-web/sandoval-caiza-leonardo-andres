0fd7499fa5ad71a0fc963aef06240eb5fb20b374
Leo2308 <leo_andres1995@hotmail.es>
2019-06-12 15:49:38 -0500

    Conexion BD

--- 01-http/02-servidor-web-nodejs/api-web/src/app.module.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TragosModule} from "./tragos/tragos.module";
+import { TypeOrmModule } from '@nestjs/typeorm'
+import {TragosEntity} from "./tragos/tragos.entity";
 
 @Module({
-  imports: [TragosModule],
+  imports: [TragosModule,
+    TypeOrmModule.forRoot({
+      name: 'default', //nombre de la cadena de conexion por defecto de TYPEORM
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test1',
+      entities: [
+          TragosEntity
+      ],
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ 01-http/02-servidor-web-nodejs/api-web/src/tragos/tragos.entity.ts
@@ -0,0 +1,44 @@
+import {Entity} from "typeorm/decorator/entity/Entity";
+import {Column} from "typeorm/decorator/columns/Column";
+import {PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('bd_trago')//nombre de la tabla
+export class TragosEntity{
+
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({
+        type: 'varchar',
+        length: 70,
+        name: 'nombre_trago',
+    })
+    nombre: string;
+
+    @Column({
+        type: 'varchar',
+        length: 10,
+        name: 'tipo_trago',
+    })
+    tipo: 'Ron'|'Vodka'|'Whiskey'|'Tequila'|'Puntas'|'Cerveza';
+
+    @Column({
+        type: 'int',
+        name: 'grados_alcohol',
+    })
+    gradosAlcohol: number;
+
+    @Column({
+        type: 'date',
+        name: 'fecha_caducidad',
+    })
+    fechaCaducidad: Date;
+
+    @Column({
+        type: 'decimal',
+        precision: 10,
+        scale:2,
+        name: 'precio',
+    })
+    precio: number;
+}
