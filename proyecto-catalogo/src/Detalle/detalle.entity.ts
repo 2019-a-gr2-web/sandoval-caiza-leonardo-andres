c33a226e795d9c3f72411840ee6a960e66975c28
Leo2308 <leo_andres1995@hotmail.es>
2019-07-30 00:12:26 -0500

    Login-Producto

---
+++ proyecto-catalogo/src/Detalle/detalle.entity.ts
@@ -0,0 +1,20 @@
+import {Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {ProductoEntity} from "../Producto/producto.entity";
+import {TiendaEntity} from "../Tienda/tienda.entity";
+import {PedidoEntity} from "../Pedido/pedido.entity";
+
+@Entity('detalle')
+export  class DetalleEntity {
+
+    @PrimaryGeneratedColumn()
+    detalleId:number;
+
+    @ManyToOne(type => ProductoEntity,
+        producto=> producto.detalles)
+    peliculaId:ProductoEntity;
+
+    @ManyToOne(type => PedidoEntity,
+        pedido=> pedido.detalles)
+    pedidoId:PedidoEntity;
+
+}
---
+++ proyecto-catalogo/src/Usuario/usuario.entity.ts
@@ -0,0 +1,44 @@
+import {Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {DetalleEntity} from "../Detalle/detalle.entity";
+import {PedidoEntity} from "../Pedido/pedido.entity";
+
+@Entity('usuario') // Nombre tabla
+export class UsuarioEntity {
+
+    @PrimaryGeneratedColumn()
+    usuarioId?:number;
+
+    @Column({
+        length:100
+    })
+    nombreUsuario?: string;
+
+    @Column({
+        length:10
+    })
+    password?: string;
+
+    @Column({
+        length:100
+    })
+    direccion?: string;
+
+    @Column({
+        length:10
+    })
+    telefono: string;
+
+    @Column({
+        length:10
+    })
+    cedula: string;
+
+    @Column({
+        type: 'int',
+    })
+    tipo?: '1'|'2'|'3';
+
+    @OneToMany(type => PedidoEntity,
+        pedido=> pedido.usuarioId)
+    pedidos:DetalleEntity[];
+}
